e2c6e59a490ab34bf85d7e902e90ca7406a49884
hidziri <hidziri@ya.ru>
2020-12-07 00:59:51 +0300

    HW1

---
+++ app/src/main/java/ru/geekbrains/dictionary/model/data/DataModel.kt
@@ -0,0 +1,8 @@
+package ru.geekbrains.dictionary.model.data
+
+sealed class DataModel {
+
+    data class Success(val data: List<SearchResult>?) : DataModel()
+    data class Error(val error: Throwable) : DataModel()
+    data class Loading(val progress: Int?) : DataModel()
+}
